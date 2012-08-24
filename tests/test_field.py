733d5b22b5092b86c0477a9be5d2110613749c30
Gustavo Narea <gustavonarea@2degreesnetwork.com>
2012-08-24 17:32:38 +0100

    Started support for input text encoded in a codec other than the one supported by reCAPTCHA

--- tests/test_field.py
+++ tests/test_field.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 ################################################################################
 #
 # Copyright (c) 2012, 2degrees Limited <2degrees-floss@googlegroups.com>.
@@ -14,13 +15,18 @@
 #
 ################################################################################
 
+import codecs
+
+from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.utils.encoding import force_unicode
 from nose.tools import assert_false
+from nose.tools import assert_not_equals
 from nose.tools import assert_raises
 from nose.tools import assert_raises_regexp
 from nose.tools import eq_
 from nose.tools import ok_
+from recaptcha import RECAPTCHA_CHARACTER_ENCODING
 from recaptcha import RecaptchaInvalidChallengeError
 from recaptcha import RecaptchaInvalidPrivateKeyError
 from recaptcha import RecaptchaUnreachableError
@@ -76,6 +82,80 @@ class TestWidgetInitialization(object):
             )
         
         ok_(field.widget.transmit_challenge_over_ssl)
+
+
+class TestFieldValueConversion(object):
+    
+    _multiprocess_can_split_ = False   # The fixtures change global data
+    
+    def setup(self):
+        self.recaptcha_client = \
+            _OfflineVerificationClient(is_solution_correct=True)
+        self.field = RecaptchaField(self.recaptcha_client, RANDOM_REMOTE_IP)
+    
+    def teardown(self):
+        settings.DEFAULT_CHARSET = 'UTF-8'
+    
+    def test_no_value(self):
+        field_value = None
+        eq_(field_value, self.field.to_python(field_value))
+    
+    def test_value_in_encoding_not_supported_by_recaptcha(self):
+        settings.DEFAULT_CHARSET = 'Latin-1'
+        self._assert_different_codecs(
+            RECAPTCHA_CHARACTER_ENCODING,
+            settings.DEFAULT_CHARSET,
+            )
+        
+        random_string_utf8 = u'profesión'.encode('UTF-8')
+        random_string_bytes = random_string_utf8.decode('UTF-8')
+        random_string_unsupported = \
+            random_string_bytes.encode(settings.DEFAULT_CHARSET)
+        field_value = {
+            'solution_text': random_string_unsupported,
+            'challenge_id': random_string_unsupported,
+            }
+        
+        self.field.validate(field_value)
+        eq_(random_string_utf8, self.recaptcha_client.solution_text)
+        eq_(random_string_utf8, self.recaptcha_client.challenge_id)
+    
+    def test_unicode_value_in_supported_codec(self):
+        settings.DEFAULT_CHARSET = RECAPTCHA_CHARACTER_ENCODING
+        
+        random_unicode_string = u'profesión'.encode(settings.DEFAULT_CHARSET)
+        field_value = {
+            'solution_text': random_unicode_string,
+            'challenge_id': random_unicode_string,
+            }
+        
+        self.field.validate(field_value)
+        eq_(random_unicode_string, self.recaptcha_client.solution_text)
+        eq_(random_unicode_string, self.recaptcha_client.challenge_id)
+    
+    def test_ascii_value(self):
+        eq_(
+            _RANDOM_RECAPTCHA_FIELD_VALUE,
+            self.field.to_python(_RANDOM_RECAPTCHA_FIELD_VALUE),
+            )
+    
+    #{ Utilities
+    
+    def _assert_different_codecs(self, codec1_name, codec2_name):
+        """Assert that ``codec1_name`` and ``codec2_name`` aren't equivalent."""
+        codec1_info = codecs.lookup(codec1_name)
+        codec2_info = codecs.lookup(codec2_name)
+        
+        assert_not_equals(
+            codec1_name,
+            codec2_name,
+            'Codecs {} and {} must be different'.format(
+                codec1_name,
+                codec2_name,
+                ),
+            )
+    
+    #}
 
 
 class TestFieldValidation(object):
@@ -162,10 +242,17 @@ class _OfflineVerificationClient(object):
         super(_OfflineVerificationClient, self).__init__()
         
         self.is_solution_correct_ = is_solution_correct
+        
         self.communication_attemps = 0
+        
+        self.solution_text = None
+        self.challenge_id = None
     
     def is_solution_correct(self, solution_text, challenge_id, remote_ip):
         self.communication_attemps += 1
+        
+        self.solution_text = solution_text
+        self.challenge_id = challenge_id
         
         return self.is_solution_correct_
 
